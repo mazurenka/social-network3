d51382a00ff8adee6301c1827cf999621771b62d
mazurenka <an.drej@mail.ru>
2022-07-27 14:16:38 +0200

    11

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,22 @@ import Footer from "./Footer";
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
-            <Footer/>
+        <div className={'app-wrapper'}>
+            <header className={'header'}>
+                <img
+                    src={'https://png.pngtree.com/png-clipart/20190604/original/pngtree-business-logo-design-png-image_915991.jpg'}/>
+            </header>
+            <nav className={'nav'}>
+                <div>
+                    Profile
+                </div>
+                <div>
+                    Messages
+                </div>
+            </nav>
+            <div className={'content'}>
+                Main Content
+            </div>
         </div>
     );
 }
