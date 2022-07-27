1039515c1ef10c1d5266828abc70082e111e4b85
mazurenka <an.drej@mail.ru>
2022-07-27 13:08:47 +0200

    9

---
+++ src/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="App">
+            <ul>
+                <li>css</li>
+                <li>js</li>
+                <li>html</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Header
---
+++ src/Technologies.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>js</li>
+                <li>html</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Technologies
