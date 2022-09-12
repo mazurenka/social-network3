3583d7f3d9f45ea01bbea363fc2c541faa498879
mazurenka <an.drej@mail.ru>
2022-09-12 16:53:52 +0200

    15

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import s from './Navbar.module.css'
 
 export const Navbar = () => {
     return <nav className={s.nav}>
-        <div className={s.item}>
+        <div className={`${s.item} ${s.active}`}>
             <a>Profile</a>
         </div>
         <div className={s.item}>
