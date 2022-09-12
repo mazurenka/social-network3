89ab2d7eaca597768f98b74f024f989f8ab7c9d2
mazurenka <an.drej@mail.ru>
2022-09-12 16:40:32 +0200

    14

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import s from './Header.module.css';
 
 export const Header = () => {
-    return <header className={'header'}>
+    return <header className={s.header}>
         <img
             src={'https://png.pngtree.com/png-clipart/20190604/original/pngtree-business-logo-design-png-image_915991.jpg'}/>
     </header>
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import s from './Profile.module.css'
 
 export const Profile = () => {
-    return  <div className={'content'}>
+    return <div className={s.content}>
         <div>
             <img
                 src={'https://images.ctfassets.net/hrltx12pl8hq/4plHDVeTkWuFMihxQnzBSb/aea2f06d675c3d710d095306e377382f/shutterstock_554314555_copy.jpg'}/>
@@ -15,12 +16,15 @@ export const Profile = () => {
             <div>
                 New Post
             </div>
-            <div>
-                pos 1
+            <div className={s.posts}>
+                <div className={s.item}>
+                    post 1
+                </div>
+                <div className={s.item}>
+                    post 2
+                </div>
             </div>
-            <div>
-                pos 2
-            </div>
+
         </div>
     </div>
 }
